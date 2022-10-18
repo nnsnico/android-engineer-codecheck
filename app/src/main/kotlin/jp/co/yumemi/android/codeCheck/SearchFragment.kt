a8e1d7f1194c2d7b9fe9aa25472a8fb8e83a3a71
nnsnico <yuji.jampan.blue@gmail.com>
2022-10-19 00:34:56 +0900

    Rename package name to camelCase

--- app/src/main/kotlin/jp/co/yumemi/android/codeCheck/SearchFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codeCheck/SearchFragment.kt
@@ -1,7 +1,7 @@
 /*
  * Copyright © 2021 YUMEMI Inc. All rights reserved.
  */
-package jp.co.yumemi.android.code_check
+package jp.co.yumemi.android.codeCheck
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -12,7 +12,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.*
-import jp.co.yumemi.android.code_check.databinding.FragmentSearchBinding
+import jp.co.yumemi.android.codeCheck.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
 
@@ -60,11 +60,17 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
 }
 
 val diffUtil = object : DiffUtil.ItemCallback<GitHubRepositoryItem>() {
-    override fun areItemsTheSame(oldItem: GitHubRepositoryItem, newItem: GitHubRepositoryItem): Boolean {
+    override fun areItemsTheSame(
+        oldItem: GitHubRepositoryItem,
+        newItem: GitHubRepositoryItem
+    ): Boolean {
         return oldItem.name == newItem.name
     }
 
-    override fun areContentsTheSame(oldItem: GitHubRepositoryItem, newItem: GitHubRepositoryItem): Boolean {
+    override fun areContentsTheSame(
+        oldItem: GitHubRepositoryItem,
+        newItem: GitHubRepositoryItem
+    ): Boolean {
         return oldItem == newItem
     }
 
