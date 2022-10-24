f059faaac55633f4c109106b9c50e010fb3aee14
nnsnico <yuji.jampan.blue@gmail.com>
2022-10-24 18:52:05 +0900

    Fix ViewModel to inject dependencies from fragment

--- app/src/main/kotlin/jp/co/yumemi/android/codeCheck/search/SearchViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codeCheck/search/SearchViewModel.kt
@@ -7,24 +7,25 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import io.ktor.client.*
-import io.ktor.client.engine.android.*
-import jp.co.yumemi.android.codeCheck.data.GitHubApi
-import jp.co.yumemi.android.codeCheck.data.GitHubApiImpl
-import jp.co.yumemi.android.codeCheck.data.GitHubRepositoryImpl
 import jp.co.yumemi.android.codeCheck.model.GitHubRepositoryItem
 import jp.co.yumemi.android.codeCheck.model.repository.GitHubRepository
 import kotlinx.coroutines.launch
 
-class SearchViewModel : ViewModel() {
-    // TODO inject from constructor by DI
-    private val client = HttpClient(Android)
-    private val api: GitHubApi = GitHubApiImpl(client)
-    private val repository: GitHubRepository = GitHubRepositoryImpl(api)
+class SearchViewModel(private val repository: GitHubRepository) : ViewModel() {
 
     private val _repositoryItems: MutableLiveData<List<GitHubRepositoryItem>> = MutableLiveData()
     val repositoryItems: LiveData<List<GitHubRepositoryItem>> = _repositoryItems
+
+    companion object {
+        fun provideFactory(repository: GitHubRepository) =
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return SearchViewModel(repository) as T
+                }
+            }
+    }
 
     fun searchResults(inputText: String) {
         viewModelScope.launch {
